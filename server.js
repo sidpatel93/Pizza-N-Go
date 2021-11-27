56d7f7daa49ded8e98e435af8aae7117bc6c8b4f
spatel93 <sid.auburn@gmail.com>
2021-11-27 19:23:27 +0000

    define initial project structure of the project

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
+const path = require('path')
 const app = express();
 const morgan = require("morgan");
 
@@ -19,6 +20,7 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
+app.set('views', path.join(__dirname, './views'));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
@@ -34,20 +36,19 @@ app.use(
 app.use(express.static("public"));
 
 // Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const adminRoutes = require("./routes/admin");
 
 // Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/widgets", adminRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+//Home page of the app. This will show the all the menu and available food options
 app.get("/", (req, res) => {
   res.render("index");
 });
