a57cd60f3cf30e4256e31cffe651a8380b7d66aa
spatel93 <sid.auburn@gmail.com>
2021-11-28 17:28:50 +0000

    fix the db seeds

--- db/schema/test_menuItems.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS menu_items CASCADE;
-CREATE TABLE menu_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  description text NOT NULL,
-  image_url text,
-  price smallint NOT NULL
-);
