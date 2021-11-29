afa3d1039ac31702ad7a3d4773b97f4443e44e18
spatel93 <sid.auburn@gmail.com>
2021-11-29 17:00:48 +0000

    working on admin orders page

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,5 +1,18 @@
+
 $(document).ready(function(){
+console.log("admin.js is loaded")
+const adminOrdersPage = $('#adminOrdersDiv')
 
-
+  axios.get('/admin/orders', {
+    Headers: {
+      "X-Requested-With": "XMLHttpRequest"
+    }
+  })
+  .then(res => {
+    data = res.data
+    generateAdminOrders = generateAdminOrders(data)
+    // Insert data in the admin/orders page
+    adminOrdersPage.innerHTML = generateElements
+  })
 
 })
