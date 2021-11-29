7dcc9d3d1caa951c76de5c997080940b56cc2dd0
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-29 06:26:45 +0000

    Added functionality to show or hide menu items from the clicked category + small style fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,19 +4,19 @@
 $(document).ready(function(){
 
   // Console.log to check if this file if loaded in browser properly
-  console.log("app.js is loaded")
+  console.log("app.js is loaded");
 
   const addInCart = (foodItem) => {
     axios.post('/cart-update', foodItem)
     .then((res) => {
-        console.log(res)
+        console.log(res);
     }).catch((err)=>{
-      console.log(err)
-    })
+      console.log(err);
+    });
   }
 
   //Select all the add to cart buttons -> this will give an array of all the btns
-  let addToCartBtn = document.querySelectorAll('.add-to-cart')
+  const addToCartBtn = document.querySelectorAll('.add-to-cart');
   // attach event listener on individual button to add it to cart
   addToCartBtn.forEach((btn)=>{
     btn.addEventListener('click', (event)=>{
@@ -24,8 +24,29 @@ $(document).ready(function(){
       addInCart(foodItem)
 
 
-    })
-  })
+    });
+  });
 
+  const $category = $('.category');
+  const $itemsContainer = $('.items-container');
+  
+  //  Expand and hide menu items based on category
+  $category.each(function() {
+    
+    // $(this) refers to which category is being clicked
+    $(this).on('click', e => {
+      e.preventDefault();
+      
+      // Check if the menu items are hidden or not and either shows them or hides them | ** needs refactoring **
+      if ($(this).find($itemsContainer).hasClass('hide')) {
+        $(this).find($itemsContainer).removeClass('hide');
+        $(this).find($itemsContainer).slideDown('fast');
+      } else {
+        $(this).find($itemsContainer).addClass('hide');
+        $(this).find($itemsContainer).slideUp('fast');
+      }
 
-})
+    });
+  });
+
+});
